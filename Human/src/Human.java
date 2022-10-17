7605ce6e779763ddfe7ebc911b867020a58e074c
Stepan <pe4enkin.stepan@yandex.ru>
2022-10-17 18:02:42 +0600

    AnimalsV2 Using constructors and objects

---
+++ Human/src/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+
+    public Human(String name, int weight){
+
+    }
+    public Human(String name, int weight, int height){
+
+    }
+    public Human(String name, int weight, int height, int age){
+
+    }
+    public Human(String name){
+
+    }
+    public Human(String name, String surname){
+
+    }
+    public Human(String name, String surname,int weight, int height, int age){
+
+    }
+}
